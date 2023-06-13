26d3d2c9bea806d8302da750d0e01bd001e782c9
@officialpaaqwsi <iklutse@st.ug.edu.gh>
2023-06-13 18:54:11 +0000

    This is the app

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+//add.addEventListener("click",() => {
+ //   count.innerHTML++;
+//});
+
+//resetCount.addEventListener("click",() => {
+    count.innerHTML = 0;
+//});
+
+//sub.addEventListener("click",() => {
+//    count.innerHTML--;
+//});
+
+
+
